4bd628f5a7984ecc631d2caf68ce7c5dd14f9483
nishalsr <nishalsr14@gmail.com>
2024-08-20 12:29:27 +0530

    Tried to use gitignore

--- CRUD_app/Server/server.js
+++ CRUD_app/Server/server.js
@@ -84,3 +84,94 @@ app.delete('/delete/:id', (req, res) => {
 app.listen(3030, () => {
     console.log("App is running")
 })
+
+
+
+// import express from 'express';
+// import mysql from 'mysql';
+// import cors from 'cors';
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// const db = mysql.createConnection({
+//     host: 'localhost', // Change this to the name of your MySQL container
+//     user: 'root',            // Your MySQL username
+//     password: 'Root@123',        // Your MySQL password
+//     database: 'crud'         // Your database name
+// });
+
+// db.connect(err => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//         return;
+//     }
+//     console.log('Connected to the MySQL database.');
+// });
+
+// app.get('/', (req, res) => {       
+//     const sql = "SELECT * FROM book";
+//     db.query(sql, (err, data) => {
+//         if (err) {
+//             console.error(err); // Log the actual error
+//             return res.json({ Error: 'Error' });
+//         }
+//         return res.json(data);
+//     });
+// });
+
+// // Insert data to the MySQL database
+// app.post('/create', (req, res) => {       
+//     const sql = "INSERT INTO book (publisher, name, date) VALUES (?)";
+
+//     const values = [
+//         req.body.publisher,
+//         req.body.name,
+//         req.body.date
+//     ];
+
+//     db.query(sql, [values], (err, data) => {
+//         if (err) {
+//             console.error(err); // Log the actual error
+//             return res.json({ Error: 'Error' });
+//         }
+//         return res.json(data);
+//     });
+// });
+
+// // Update a record
+// app.put('/update/:id', (req, res) => {       
+//     const sql = "UPDATE book SET publisher = ?, name = ?, date = ? WHERE id = ?";
+
+//     const values = [
+//         req.body.publisher,
+//         req.body.name,
+//         req.body.date
+//     ];
+//     const id = req.params.id;
+//     db.query(sql, [...values, id], (err, data) => {
+//         if (err) {
+//             console.error(err); // Log the actual error
+//             return res.json({ Error: 'Error' });
+//         }
+//         return res.json(data);
+//     });
+// });
+
+// // Delete a record
+// app.delete('/delete/:id', (req, res) => {       
+//     const sql = "DELETE FROM book WHERE id = ?";
+//     const id = req.params.id;
+//     db.query(sql, [id], (err, data) => {
+//         if (err) {
+//             console.error(err); // Log the actual error
+//             return res.json({ Error: 'Error' });
+//         }
+//         return res.json(data);
+//     });
+// });
+
+// app.listen(3030, () => {
+//     console.log("App is running on http://localhost:3030");
+// });

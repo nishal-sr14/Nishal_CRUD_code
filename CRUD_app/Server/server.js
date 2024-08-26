1c9b24d55711fb94e34703920482b333c6a3a655
Nishal.SR <nishal.sr@impressico.com>
2024-08-26 16:10:02 +0530

    Cleared some issues in code

--- CRUD_app/Server/server.js
+++ CRUD_app/Server/server.js
@@ -1,17 +1,17 @@
-import express from 'express'  // If you want to import like this, you need to add a entry "type" : "module" in package,json file
-import mysql from 'mysql'
-import cors from 'cors'  //These are used for cors origin to access our server side app from frontend
+import express from 'express';  // If you want to import like this, you need to add a entry "type" : "module" in package.json file
+import mysql from 'mysql';
+import cors from 'cors';  // These are used for CORS origin to access our server-side app from frontend
 
-const app = express()
-app.use(express.json())   //This will convert the data we transfer to the JSON format
-app.use(cors())
+const app = express();
+app.use(express.json());   // This will convert the data we transfer to the JSON format
+app.use(cors());
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Root@123',
     database: 'crud'
-})
+});
 
 db.connect(err => {
     if (err) {
@@ -21,7 +21,7 @@ db.connect(err => {
     console.log('Connected to the MySQL database.');
 });
 
-//'req' variable we pass it from frontend 'res' : whatever we return back to the frontend
+// 'req' variable we pass it from frontend 'res' : whatever we return back to the frontend
 app.get('/', (req, res) => {
     const sql = "SELECT * FROM book";
     db.query(sql, (err, data) => {
@@ -33,31 +33,15 @@ app.get('/', (req, res) => {
     });
 });
 
-//Insert data to the mysql database
-<<<<<<< HEAD
-app.post('/create', (req, res) => {
-=======
-
+// Insert data to the MySQL database
 app.post('/create', (req, res) => {
     // Define SQL query with placeholders
->>>>>>> e4aee19 (Modified code and added delete button)
     const sql = "INSERT INTO book (publisher, name, date) VALUES (?)";
 
     const values = [
         req.body.publisher,
         req.body.name,
         req.body.date
-<<<<<<< HEAD
-    ]
-
-    db.query(sql, [values], (err, data) => {
-        if (err) {
-            return res.json({ Error: 'Error' })
-        }
-        return res.json(data)
-    })
-})
-=======
     ];
 
     db.query(sql, [values], (err, result) => {
@@ -79,39 +63,13 @@ app.post('/create', (req, res) => {
     });
 });
 
->>>>>>> e4aee19 (Modified code and added delete button)
-
-//To update the code    //whenever updating a record we need to pass ID
+// To update the code    // whenever updating a record we need to pass ID
 app.put('/update/:id', (req, res) => {
     const sql = "UPDATE book SET publisher = ?, name = ?, date = ? WHERE id = ?";
 
     const values = [
         req.body.publisher,
         req.body.name,
-<<<<<<< HEAD
-        req.body.date
-    ]
-    const id = req.params.id;
-    db.query(sql, [...values, id], (err, data) => {
-        if (err) {
-            return res.json({ Error: 'Error' })
-        }
-        return res.json(data)
-    })
-})
-
-//To delete a record
-app.delete('/delete/:id', (req, res) => {
-    const sql = "DELETE FROM book WHERE id = ?";
-    const id = req.params.id;
-    db.query(sql, [id], (err, data) => {
-        if (err) {
-            return res.json({ Error: 'Error' })
-        }
-        return res.json(data)
-    })
-})
-=======
         req.body.date,
         req.params.id  
     ];
@@ -136,8 +94,7 @@ app.delete('/delete/:id', (req, res) => {
     });
 });
 
-
-//To delete a record
+// To delete a record
 app.delete('/delete/:id', (req, res) => {
     const id = req.params.id;
 
@@ -156,9 +113,6 @@ app.delete('/delete/:id', (req, res) => {
     });
 });
 
->>>>>>> e4aee19 (Modified code and added delete button)
-
 app.listen(3030, () => {
-    console.log("App is running")
-})
-
+    console.log("App is running");
+});
